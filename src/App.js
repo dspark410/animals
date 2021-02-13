c1c6566aec0e0f0cf8b8ec38cda1677d4c4a156b
mistahpig <dspark410@gmail.com>
2021-02-12 20:17:38 -0800

    adds grid layout

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import { videoData } from './constant'
 
 function App() {
   return (
-    <div className='video-container'>
-      {videoData.map((animal, i) => {
-        return (
-          <iframe
-            className='video'
-            key={i}
-            src={animal.video}
-            frameBorder='0'
-            allow='autoplay; encrypted-media'
-            allowFullScreen
-            title='video'
-          />
-        )
-      })}
-    </div>
+    <>
+      <h1>Make Me Laugh</h1>
+      <div className='video-container'>
+        {videoData.map((animal, i) => {
+          return (
+            <iframe
+              className='video'
+              key={i}
+              src={animal.video}
+              frameBorder='0'
+              allow='autoplay; encrypted-media'
+              allowFullScreen
+              title='video'
+            />
+          )
+        })}
+      </div>
+    </>
   )
 }
 
